b75f66fb0fdc88d7f9d55e5753cefe19c4198aac
Joey David <doey08@gmail.com>
2021-11-06 14:53:38 -0400

    list selector

--- ui/src/app/modules/shopping-list/components/shopping-list.component.ts
+++ ui/src/app/modules/shopping-list/components/shopping-list.component.ts
@@ -1,29 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ListEntry } from '../models/ListEntry';
+import { List } from '../models/List';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
 
-  public listEntry: FormGroup = new FormGroup({
+  public formListEntry: FormGroup = new FormGroup({
     item: new FormControl('', [Validators.required]),
     quantity: new FormControl(1, [Validators.required]),
   });
 
-  public listEntries: ListEntry[] = [];
+  public lists: List[] = []
+  public activeList!: List;
 
   constructor() { }
 
+  ngOnInit() {
+    // TODO get lists from storage
+    this.lists.push({name: 'My List', entries:[]});
+    this.lists.push({name: 'Another List', entries:[]});
+    this.activeList = this.lists[0];
+  }
+
+  public onSelectListChange(value: List) {
+    this.activeList = value;
+  }
+
   public onSubmitListEntry() {
     const entry: ListEntry = {
-      item: this.listEntry.controls['item'].value,
-      quantity: this.listEntry.controls['quantity'].value
-    }
-    this.listEntries.push(entry);
-    console.log(this.listEntries);
+      item: this.formListEntry.controls['item'].value,
+      quantity: this.formListEntry.controls['quantity'].value
+    };
+    this.activeList.entries.push(entry);
   }
 }
--- ui/src/app/shared.module.ts
+++ ui/src/app/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from "@angular/material/input";
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 const materialModules = [
   MatToolbarModule,
@@ -13,6 +15,7 @@ const materialModules = [
   MatButtonModule,
   MatFormFieldModule,
   MatInputModule,
+  MatSelectModule,
 ]
 
 const sharedModules = [
