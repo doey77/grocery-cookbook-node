521ba575deaac082cf674967d15bdff49ab2b26d
Joey David <doey08@gmail.com>
2021-11-13 12:08:32 -0500

    navbar into own component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -10,12 +9,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared.module';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  template: `<app-navbar></app-navbar>
+  <router-outlet></router-outlet>`
+}) class AppComponent { }
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    NotfoundComponent
+    NotfoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
