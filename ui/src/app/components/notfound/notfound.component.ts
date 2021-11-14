acdef4f0b4a3c59726abf324f6cf854ab75a84e9
Joey David <doey08@gmail.com>
2021-11-14 13:57:11 -0500

    404 styling

--- ui/src/app/components/notfound/notfound.component.ts
+++ ui/src/app/components/notfound/notfound.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notfound',
-  template: `The requested page could not be found`,
+  template: `<div class="center"><h1>The requested page could not be found</h1></div>`,
+  styles: [`
+  .center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30%;
+    text-align: center;
+  }
+  `]
 })
 export class NotfoundComponent {}
