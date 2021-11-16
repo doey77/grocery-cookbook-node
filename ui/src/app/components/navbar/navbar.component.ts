6eb66ec50bb643ec76c908de2271bf75706e6849
Joey David <doey08@gmail.com>
2021-11-16 15:38:20 -0500

    major navbar adjustments

--- ui/src/app/components/navbar/navbar.component.ts
+++ ui/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { links } from 'src/app/links';
+
+type BottomClass = 'navBtnsBottomStick' | 'navBtnsBottomRelative'
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   public links = links;
+  public bottomClass: BottomClass = 'navBtnsBottomStick';
+  public shortSidebar = false;
+
+  private heightBreakpoint = 400;
+
+  ngOnInit() {
+    this.setBottomItemClass(window);
+  }
+
+  toggleDrawerWidth() {
+    this.shortSidebar = !this.shortSidebar;
+  }
+
+  onResize(event: any) {
+    this.setBottomItemClass(event.target);
+  }
+
+  setBottomItemClass(window: Window) {
+    this.bottomClass = window.innerHeight <= this.heightBreakpoint ?
+      'navBtnsBottomRelative' : 'navBtnsBottomStick' 
+  }
 }
