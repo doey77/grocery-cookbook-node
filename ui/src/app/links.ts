d1ec2f68d144c192144400033e811ce11ad6f669
Joey David <doey08@gmail.com>
2021-11-16 16:17:12 -0500

    navbar icons

--- ui/src/app/links.ts
+++ ui/src/app/links.ts
@@ -1,23 +1,27 @@
 export interface ILink {
     displayText: string,
     routerLink: string,
-    homepageImgLink?: string
+    homepageImgLink?: string,
+    matIcon?: string,
 }
 
 // Currently for internal navigation only
 export const links: ILink[] = [
     {
         routerLink: '/',
-        displayText: 'Home'
+        displayText: 'Home',
+        matIcon: 'home'
     },
     {
         routerLink: '/shoppinglist',
         displayText: 'Shopping Lists',
-        homepageImgLink: '/assets/shopping-list.jpg'
+        homepageImgLink: '/assets/shopping-list.jpg',
+        matIcon: 'local_grocery_store'
     },
     {
         routerLink: '/recipes',
         displayText: 'Recipes',
-        homepageImgLink: '/assets/recipes.jpg'
+        homepageImgLink: '/assets/recipes.jpg',
+        matIcon: 'edit_note'
     },
 ];
