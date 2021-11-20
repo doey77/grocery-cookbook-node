f0a8ee11d1c50251830ac89a54a503b42a5857e4
Joey David <doey08@gmail.com>
2021-11-20 13:47:04 -0500

    'msg'

--- api/src/middleware/errorHandler.ts
+++ api/src/middleware/errorHandler.ts
@@ -10,13 +10,13 @@ const errorHandler = (err: unknown, req: Request,
     if (err instanceof ValidateError) {
         console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
         return res.status(422).json({
-        message: "Validation Failed",
+        msg: "Validation Failed",
         details: err?.fields,
         });
     }
     if (err instanceof Error) {
         return res.status(500).json({
-        message: "Internal Server Error",
+        msg: "Internal Server Error",
         });
     }
 
--- api/src/middleware/notFoundHandler.ts
+++ api/src/middleware/notFoundHandler.ts
@@ -5,7 +5,7 @@ import { Request, Response } from 'express';
  */
 const notFoundHandler = (req: Request, res: Response) => {
     res.status(404).send({
-      message: "Not Found",
+      msg: "Not Found",
     });
 };
 
