e9fcd36a3077001f42d084869c7bb7dd3f022015
Joey David <doey08@gmail.com>
2021-12-12 13:13:29 -0500

    start of login

--- api/src/services/user.ts
+++ api/src/services/user.ts
@@ -1,5 +1,8 @@
 import { FindManyOptions, getManager } from "typeorm";
 import { UserDB } from "../db/models/User";
+import { sign } from "jsonwebtoken";
+import env from "../env";
+import { RequestError } from "../misc/errors";
 
 export type userGetArgs = {
   id: number
@@ -11,13 +14,33 @@ const getMany = (args?: FindManyOptions) => getManager().getRepository(UserDB).f
 
 export type userCreateArgs = Pick<UserDB, "email" | "password" >;
 
+// TODO encrypt passwords
 const create = (args: userCreateArgs) => getManager().save(
     new UserDB(args.email, args.password)
   ).then(user => user);
+
+export type userLoginArgs = {
+  email: string,
+  password: string,
+}
+
+const login = async (args: userLoginArgs) => {
+  const userRepo = (await getManager().getRepository(UserDB).find({email:args.email}));
+
+  if (userRepo.length === 1) {
+    const user = userRepo[0];
+    if (user.password === args.password) {
+      return sign(user.email, env.authTokenSecret, {expiresIn: 3*86400});
+    }
+  }
+
+  throw new RequestError(400,'Username or password was incorrect');
+}
 
 
 export const userService = {
   get: get,
   getMany: getMany,
-  create: create
+  create: create,
+  login: login
 }
